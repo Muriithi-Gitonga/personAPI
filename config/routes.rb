f4de6ecdaeb1123bfba6fd8035a7617e29c781f7
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-09-11 12:25:43 +0300

    remove index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  get "api", to: "person#index"
   get "api/:user_id", to: "person#show"
   post "api", to: "person#create"
   put "api/:user_id", to: "person#update"
